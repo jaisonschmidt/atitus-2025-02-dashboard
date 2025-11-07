da95296c60129ab6a2bef1aab54e0d372fbf7610
Jaison Schmidt <31486070+jaisonschmidt@users.noreply.github.com>
2025-11-07 23:27:16 +0000

    feat: adicionar página de dashboard e configurar roteamento

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import { Dashboard } from "./pages/Dashboard";
 import { Login } from "./pages/Login"
 
 function App() {
-  return <Login />;
+  return <Dashboard />;
 }
 
 export default App
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+export function Dashboard() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>Bem-vindo ao dashboard! Aqui você pode acessar as principais funcionalidades do sistema.</p>
+        </div>
+    );
+}
