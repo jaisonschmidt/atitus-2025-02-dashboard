d0ed24e05cafc2cdf78415a371090a93ca9e970a
Jaison Schmidt <31486070+jaisonschmidt@users.noreply.github.com>
2025-11-07 23:40:16 +0000

    feat: implementar redirecionamento para a página de login no componente Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,24 @@
+/**
+ * No load do componente, pegar o valor de isLogged
+ * Se for false, redirecionar para a página de login
+ * Se for true, mostrar o conteúdo do dashboard
+ */
+import { useEffect } from "react";
+import { useAuth } from "../contexts/AuthContext.jsx";
+import { useNavigate } from "react-router-dom";
+
 export function Dashboard() {
+    const { isLogged } = useAuth();
+    const navigate = useNavigate();
+
+    // ocorre no load do componente
+    useEffect(() => {
+        if (!isLogged) {
+            // redirecionar para a página de login
+            navigate("/");
+        }
+    }, []);
+
     return (
         <div>
             <h1>Dashboard</h1>
