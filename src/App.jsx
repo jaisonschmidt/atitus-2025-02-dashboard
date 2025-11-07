a6bc654ffd1a2591b3e2ceda7bca5c731e89d6a6
Jaison Schmidt <31486070+jaisonschmidt@users.noreply.github.com>
2025-11-07 23:32:21 +0000

    feat: configurar roteamento para as páginas de Login e Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 import { Dashboard } from "./pages/Dashboard";
 import { Login } from "./pages/Login"
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />
+  }
+]);
+
 function App() {
-  return <Dashboard />;
+  return <RouterProvider router={router} />;
 }
 
 export default App
