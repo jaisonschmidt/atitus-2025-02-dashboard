5744a772590d6fc31cfa5658c5b6fe31ef869fcf
Jaison Schmidt <31486070+jaisonschmidt@users.noreply.github.com>
2025-11-07 23:15:47 +0000

    feat: criar contexto de autenticação com provider e hooks

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useContext, useState } from "react";
+
+// Criação do contexto
+const AuthContext = createContext();
+
+// Hook para usar o contexto
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+    const [isLogged, setIsLogged] = useState(false);
+
+    function doLogin() {
+        setIsLogged(true);
+    }
+
+    function doLogout() {
+        setIsLogged(false);
+    }
+
+    return (
+        <AuthContext.Provider value={{ doLogin, doLogout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
